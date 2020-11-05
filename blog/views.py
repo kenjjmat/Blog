68d103ed49281be9f1652d1f5e25a2fa4d2ad80f
Kennedy J J Maturure <kenjjmat@outlook.com>
2020-11-05 16:45:09 +0200

    have connected the app to the database for Post created the table Post

--- blog/views.py
+++ blog/views.py
@@ -1,26 +1,11 @@
 from django.shortcuts import render
-
-
-# this is a list that comprises of dictionaries
-posts = [
-    {
-        'author': 'CoreyMs',
-        'title': 'Blog Post 1',
-        'content': 'First post content',
-        'date_posted': 'August 27, 2018'
-    },
-    {
-        'author': 'Jane Doe',
-        'title': 'Blog Post 2',
-        'content': 'Second post content',
-        'date_posted': 'August 28, 2018'
-    }
-]
+# database
+from .models import Post
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
